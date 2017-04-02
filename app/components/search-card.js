182c728a76ce94e621c5c306d9e9729ac962f78e
Yana Agun Siswanto <yana.developer@gmail.com>
2017-04-02 11:02:38 +0700

    Extract Search card into component

--- app/components/search-card.js
+++ app/components/search-card.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    searchIssues(query) {
+      this.get('onSearch')(query);
+    }
+  },
+  searchParams: Ember.computed('params.q', function() {
+    return this.get('params')['q'];
+  })
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,9 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    q: {
+      as: 'query'
+    }
+  },
   actions: {
     searchIssues(query) {
       this.transitionTo('issues', { queryParams: { q: query} });
     }
+  },
+  model(params) {
+    return params;
   }
 });
