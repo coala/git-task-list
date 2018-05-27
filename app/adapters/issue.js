e706d74cb457965a001d0abba68970e55c0970ad
Yana Agun Siswanto <yana.developer@gmail.com>
2018-05-27 11:54:47 +0700

    Fix deprecation warning
    Update ember to update deprecated
    dependency used by ember.

--- app/adapters/issue.js
+++ app/adapters/issue.js
@@ -1,22 +1,25 @@
-import Ember from 'ember';
+import { assign } from '@ember/polyfills';
 import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
   host: 'https://api.github.com',
-  defaults: {
-    sort: 'created',
-    order: 'desc',
-    q: 'state:open'
+  init() {
+    this._super();
+    this.defaults = {
+      sort: 'created',
+        order: 'desc',
+        q: 'state:open'
+    };
   },
   query(store, type, query) {
-    const q = query.q || ''
+    const q = query.q || '';
     if(!(q).match(this.defaults.q || '')) {
       query.q = q + ` ${this.defaults.q}`
     }
     return this._super(
       store,
       type,
-      Ember.assign({}, this.defaults, query)
+      assign({}, this.defaults, query)
     );
   },
 });
